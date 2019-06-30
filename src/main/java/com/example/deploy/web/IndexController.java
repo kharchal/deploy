ee3829239091979dac977a370398acee293a1051
hav@ukr.net <githav123>
2019-06-30 21:26:31 +0300

    dev profile added

--- src/main/java/com/example/deploy/web/IndexController.java
+++ src/main/java/com/example/deploy/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.deploy.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import java.time.LocalDateTime;
 
 @Controller
 public class IndexController {
+
+    @Value("${some.param}")
+    private String name;
 
     @GetMapping({"/", "/index"})
     public String index(Model model) {
@@ -18,6 +22,7 @@ public class IndexController {
     @GetMapping("/page")
     public String page(Model model) {
         model.addAttribute("msg", "That's it.");
+        model.addAttribute("name", name);
         return "page";
     }
 }
